af62565d455b309149125bf4c82e61e4ee83d046
laurasgm <laurasophia.gonzalez@utp.edu.co>
2023-09-05 22:30:33 -0500

    class 17

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import MainLayout from '@layout/MainLayout';
+import '@styles/tailwind.css';
+import { ProviderAuth } from '@hooks/useAuth';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ProviderAuth>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ProviderAuth>
+    </>
+  );
 }
